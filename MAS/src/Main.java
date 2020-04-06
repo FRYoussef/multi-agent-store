594d99a716a8fd7bfab261d49a5c27f81e1f3e5b
FRYoussef <f.r.youssef@hotmail.com>
2020-04-06 18:12:14 +0200

    WIP adding JADE

---
+++ MAS/src/Main.java
@@ -0,0 +1,9 @@
+import agents.guiAgent.GuiAgent;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Create agents
+        new GuiAgent();
+    }
+}
--- MAS/src/agents/guiAgent/control/Gui.java
+++ MAS/src/agents/guiAgent/control/Gui.java
@@ -1,4 +1,4 @@
-package gui;
+package agents.guiAgent.control;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -6,18 +6,17 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class Gui extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../views/sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
 
-
     public static void main(String[] args) {
-        launch(args);
+        Application.launch(args);
     }
 }
