b9da607bf02dc973c07c3f3a2dabbf126b3292ee
FRYoussef <f.r.youssef@hotmail.com>
2020-04-07 16:28:19 +0200

    Added main view

--- MAS/src/agents/guiAgent/control/Controller.java
+++ MAS/src/agents/guiAgent/control/Controller.java
@@ -1,22 +1,41 @@
 package agents.guiAgent.control;
 
 import agents.guiAgent.GuiAgent;
-import jade.gui.GuiEvent;
-import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 
 public class Controller {
     private GuiAgent agent;
+
+    @FXML
+    private Button _btLeft;
+    @FXML
+    private Button _btRight;
+    @FXML
+    private HBox _hbImages;
+    @FXML
+    private Label _lbDescription;
+    @FXML
+    private TextArea _taPrompt;
+    @FXML
+    private TextField _tfInput;
+
 
     public void setAgent(GuiAgent agent) {
         this.agent = agent;
     }
 
-    @FXML
-    public void exitApplication(ActionEvent event) {
-        GuiEvent ev = new GuiEvent(this, GuiAgent.CMD_EXIT);
-        agent.postGuiEvent(ev);
-        Platform.exit();
+
+    public void onClickPager(ActionEvent actionEvent) {
+
+    }
+
+    public void onClickSend(ActionEvent actionEvent) {
+
     }
 }
