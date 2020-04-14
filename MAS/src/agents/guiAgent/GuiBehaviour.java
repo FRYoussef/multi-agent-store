09674e7f73f135d43087992878d8e9275a3dde76
raqblanc <raquel.blanco.morago@gmail.com>
2020-04-14 22:53:06 +0200

    Gui behaviour class added

---
+++ MAS/src/agents/guiAgent/GuiBehaviour.java
@@ -0,0 +1,22 @@
+package agents.guiAgent;
+import jade.core.Agent;
+import jade.core.behaviours.SimpleBehaviour;
+
+public class GuiBehaviour extends SimpleBehaviour {
+    private static final long serialVersionUID = 1L;
+
+    public GuiBehaviour(Agent a) {
+        super(a);
+    }
+
+
+    @Override
+    public void action() {
+
+    }
+
+    @Override
+    public boolean done() {
+        return false;
+    }
+}
