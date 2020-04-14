1f02389a597ca3ce2a662d56b9d7440c675d707c
raqblanc <raquel.blanco.morago@gmail.com>
2020-04-14 21:43:49 +0200

    Chatbot classes added

---
+++ MAS/src/agents/chatbotAgent/ChatbotBehaviour.java
@@ -0,0 +1,24 @@
+package agents.chatbotAgent;
+
+import jade.core.Agent;
+import jade.core.behaviours.SimpleBehaviour;
+
+public class ChatbotBehaviour extends SimpleBehaviour {
+
+    private static final long serialVersionUID = 1L;
+
+    public ChatbotBehaviour(Agent a) {
+        super(a);
+    }
+
+
+    @Override
+    public void action() {
+
+    }
+
+    @Override
+    public boolean done() {
+        return false;
+    }
+}
