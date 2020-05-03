f97079a945c4d44e44109d4548806955b9319300
raqblanc <raquel.blanco.morago@gmail.com>
2020-05-03 21:13:53 +0200

    Python script (chatbot) integrated

---
+++ MAS/chatbot.py
@@ -0,0 +1,38 @@
+import os
+import dialogflow_v2
+import sys
+
+from google.api_core.exceptions import InvalidArgument
+
+#os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = 'credentials.json'
+
+DIALOGFLOW_PROJECT_ID = 'xenon-height-273419'
+DIALOGFLOW_LANGUAGE_CODE = 'en'
+SESSION_ID = sys.argv[1]
+
+
+
+session_client = dialogflow_v2.SessionsClient()
+session = session_client.session_path(DIALOGFLOW_PROJECT_ID, SESSION_ID)
+
+text_to_be_analyzed = sys.argv[2]
+text_input = dialogflow_v2.types.TextInput(text=text_to_be_analyzed, language_code=DIALOGFLOW_LANGUAGE_CODE)
+query_input = dialogflow_v2.types.QueryInput(text=text_input)
+try:
+	response = session_client.detect_intent(session=session, query_input=query_input)
+
+except InvalidArgument:
+	raise
+
+#print('Query text: {}'.format(response.query_result.query_text))
+#print('Detected intent: {} (confidence: {})\n'.format(
+#response.query_result.intent.display_name,
+#response.query_result.intent_detection_confidence))
+#print('Chatbot> {}\n'.format(response.query_result.fulfillment_text))
+f = open("result.txt", "w")
+if response.query_result.all_required_params_present and response.query_result.intent.display_name == "Buy item":
+	f.write("END\n" + format(response.query_result.fulfillment_text))
+else:
+    f.write("CONTINUE\n" + format(response.query_result.fulfillment_text))
+f.close()
+
--- MAS/src/agents/chatbotAgent/ChatbotBehaviour.java
+++ MAS/src/agents/chatbotAgent/ChatbotBehaviour.java
@@ -4,12 +4,22 @@ import jade.core.Agent;
 import jade.core.behaviours.CyclicBehaviour;
 import jade.lang.acl.ACLMessage;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Random;
+import java.util.Scanner;
+import java.io.IOException;
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
 public class ChatbotBehaviour extends CyclicBehaviour {
 
     private static final long serialVersionUID = 1L;
-
+    private static int user_id;
     public ChatbotBehaviour(Agent a) {
         super(a);
+        Random rand = new Random();
+        this.user_id = rand.nextInt();
     }
 
 
@@ -19,8 +29,49 @@ public class ChatbotBehaviour extends CyclicBehaviour {
         if(msg != null){
             String content = msg.getContent();
             ACLMessage reply = msg.createReply();
+            String answer = "Connection failed";
+            String user_msg = msg.getContent();
+            Boolean end = false;
+            try {
+                /*Process proc = Runtime.getRuntime().exec(new String[] {"which", "python3"});
+                BufferedReader input =
+                new BufferedReader(new InputStreamReader(proc.getInputStream()));
+        BufferedReader error =
+                new BufferedReader(new InputStreamReader(proc.getErrorStream()));
+
+                String line = null;
+                while ((line = input.readLine()) != null) {
+                    System.out.println(line);
+                }
+                while ((line = error.readLine()) != null) {
+                    System.out.println(line);
+                }
+
+            */
+               /* ProcessBuilder pb = new ProcessBuilder();
+                pb.command("chatbot.py", "run", "" + this.id_user, user_msg);
+                Process p = pb.start();
+                p.waitFor();
+                ProcessHandle.Info a = p.info();*/
+
+                Process api_request = Runtime.getRuntime().exec("python3 chatbot.py " + this.user_id + " " + user_msg);
+                api_request.waitFor();
+
+                File fresult = new File("result.txt");
+                Scanner reader = new Scanner(fresult);
+                end = reader.nextLine().equals("END");
+                answer = reader.nextLine();
+            } catch (IOException | InterruptedException e) {
+                e.printStackTrace();
+            }
+
             reply.setPerformative(ACLMessage.INFORM);
-            reply.setContent("Hello!");
+            reply.setContent(answer);
+
+            if(end) {
+                System.out.print("END");
+                //Now, analyze the "answer"
+            }
             myAgent.send(reply);
         }
         else {
